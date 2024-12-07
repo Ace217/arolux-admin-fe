879acc12e18538493655e6b4f3e1d4338e5b2c27
zeeeforzain <zeeforzain@gmail.com>
2024-12-07 14:02:35 +0500

    7December,2024

--- src/Components/Figures.js
+++ src/Components/Figures.js
@@ -11,10 +11,16 @@ export default function Figures(props) {
     // borderRadius='10px'
     backgroundColor='var(--white)'
     display='flex'
-    flexDirection='column'
     justifyContent='space-between'
      boxShadow="1px 1px 1px 1px var(--secondary)"
     >
+      <BoxComponent
+      
+      display='flex'
+    flexDirection='column'
+    justifyContent='space-between'
+    gap="10px"
+      >
       <TypographyComponent
       fontSize='20px'
       fontWeight='400'
@@ -22,17 +28,27 @@ export default function Figures(props) {
       textAlign='left'
       color='var(--dark)'
       >{props.numbers}</TypographyComponent>
-      <BoxComponent
-      display='flex'
-      justifyContent='left'
-      >
       <TypographyComponent
       fontSize='14px'
       fontFamily='var(--main)'
       color='var(--paragraph)'
       textAlign='left'
       marginRight='10px'
-      >{props.title}</TypographyComponent>
+      >{props.title}</TypographyComponent> 
+      </BoxComponent>
+      <BoxComponent
+      width="20%"
+      height="95%"
+      overflow="hidden"
+      
+      >
+      <img 
+          style={{
+            width: '100%',
+            height: '100%'
+          }}
+          src={props.img} alt={props.alt}
+        />
       </BoxComponent>
     </BoxComponent>
   );
--- src/Components/FiguresBox.js
+++ src/Components/FiguresBox.js
@@ -9,9 +9,9 @@ export default function FiguresBox() {
     justifyContent='space-between'
     padding='20px'
     >
-      <Figures title='Total Revenue Generated' numbers='$ 1221323'/>
-      <Figures title='Number of Completed Rides' numbers='140'/>
-      <Figures title='Number of Current Rides' numbers='122'/>
+      <Figures title='Total Revenue Generated' numbers='$ 1221323' img="Images/revenue.png"/>
+      <Figures title='Number of Completed Rides' numbers='140' img="Images/completed-ride.png"/>
+      <Figures title='Number of Current Rides' numbers='122' img="Images/current-ride.png"/>
     </BoxComponent>
   );
 }
