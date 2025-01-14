afda4854e4d911139a163427a94073cb503a1c1c
zeeeforzain <zeeforzain@gmail.com>
2025-01-14 15:00:15 +0500

    api changed

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -5,3 +5,10 @@ export const loginAdmin = async (email, password) => {
     const response = await api.post('/admin/login', { email, password });
     return response.data; // Returns token and message
 };
+
+
+// Create Admin
+export const createAdmin = async (formData) => {
+    const response = await api.post('/admin/account', formData);
+    return response.data; // Returns the response from the API (e.g., success message, created admin data)
+};
